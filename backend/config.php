3870dc165a8436acbb1916fd91020b731bfda794
joffrey-brzoza <joffrey.brzoza@adyen.com>
2023-03-02 15:17:56 +0900

    adjust config

---
+++ backend/config.php
@@ -0,0 +1,7 @@
+<?php
+
+$config["APIKey_AdyenAPACEvent_SG_RiskGame"] = $_ENV['APIKey_AdyenAPACEvent_SG_RiskGame'];
+$config["APIKey_RiskGame_Terminals"] = $_ENV['APIKey_RiskGame_Terminals'];
+$config["MerchantAccount"] = $_ENV['MerchantAccount'];
+
+?>
--- backend/getAuthorization.php
+++ backend/getAuthorization.php
@@ -1,5 +1,5 @@
 <?php
-include_once './config.php';
+require_once('./config.php');
 
 $rawData = json_decode(file_get_contents('php://input'), true);
 
